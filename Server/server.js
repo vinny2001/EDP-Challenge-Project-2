2c4f43f713a2290fe9c4a1abfe9599aad83aab06
Rick Bernard <rick.bernard527@gmail.com>
2024-07-01 16:41:19 -0400

    Added characters/films environment variables

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,35 @@ const PORT = 3000;
 
 const url = process.env.MONGO_DB_URL;
 const dbName = process.env.MONGO_DB;
+const charactersCollection = process.env.CHARACTERS_COLLECTION;
+const filmsCollection = process.env.FILMS_COLLECTION;
 const planetsCollection = process.env.PLANETS_COLLECTION;
+
+app.get('/api/characters', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const characters_collection = db.collection(charactersCollection);
+        const characters = await characters_collection.find({}).toArray();
+        res.json(characters);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Execute order 66 ☹");
+    }
+});
+
+app.get('/api/films', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const films_collection = db.collection(filmsCollection);
+        const films = await films_collection.find({}).toArray();
+        res.json(films);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Execute order 66 ☹");
+    }
+});
 
 app.get('/api/planets', async (req, res) => {
     try {
